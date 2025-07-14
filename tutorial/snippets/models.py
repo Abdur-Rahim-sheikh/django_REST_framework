ec3a323b56c882087abe20be6f79e242861fb542
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-07-14 11:41:18 +0600

    *format

--- tutorial/snippets/models.py
+++ tutorial/snippets/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from pygments.lexers import get_all_lexers
 from pygments.styles import get_all_styles
+
 # Create your models here.
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
@@ -17,6 +18,10 @@ class Snippet(models.Model):
         choices=LANGUAGE_CHOICES, default="python", max_length=100
     )
     style = models.CharField(choices=STYLE_CHOICES, default="friendly", max_length=100)
+    highlighted = models.TextField()
+    owner = models.ForeignKey(
+        "auth.User", related_name="snippets", on_delete=models.CASCADE
+    )
 
     class Meta:
         ordering = ["created"]
