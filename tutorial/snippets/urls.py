0f5308f5d2efaf024d1735a7b7c11f835deeb95a
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-07-15 19:52:24 +0600

    onwer added in snippetSerializer

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from snippets.views import SnippetDetail, SnippetList
+from snippets.views import SnippetDetail, SnippetList, UserDetail, UserList
 
 urlpatterns = [
     path("snippets/", SnippetList.as_view(), name="snippets"),
     path("snippets/<int:pk>/", SnippetDetail.as_view(), name="snippet_details"),
+    path("users/", UserList.as_view()),
+    path("users/<int:pk>/", UserDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
