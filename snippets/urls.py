1d9bb7209f3860707b480d4617aeba2ef8b9c04c
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-07-16 20:19:38 +0600

    relating models with hyperlinked api rather primary key

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,13 +1,26 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from snippets.views import SnippetDetail, SnippetList, UserDetail, UserList
+from snippets.views import (
+    SnippetDetail,
+    SnippetHighlight,
+    SnippetList,
+    UserDetail,
+    UserList,
+    api_root,
+)
 
 urlpatterns = [
-    path("snippets/", SnippetList.as_view(), name="snippets"),
-    path("snippets/<int:pk>/", SnippetDetail.as_view(), name="snippet_details"),
-    path("users/", UserList.as_view()),
-    path("users/<int:pk>/", UserDetail.as_view()),
+    path("", api_root),
+    path("snippets/", SnippetList.as_view(), name="snippet-list"),
+    path("snippets/<int:pk>/", SnippetDetail.as_view(), name="snippet-detail"),
+    path(
+        "snippets/<int:pk>/highlight/",
+        SnippetHighlight.as_view(),
+        name="snippet-highlight",
+    ),
+    path("users/", UserList.as_view(), name="user-list"),
+    path("users/<int:pk>/", UserDetail.as_view(), name="user-detail"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
