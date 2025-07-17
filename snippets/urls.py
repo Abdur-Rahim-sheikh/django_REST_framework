79b0f9c0143d7d11b8b6ac47e3206ee5fb4af169
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-07-17 14:05:59 +0600

    added return type as static renderer

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-
+from rest_framework import renderers
 from snippets.views import (
     SnippetViewSet,
     UserViewSet,
@@ -13,7 +13,9 @@ snippet_detail = SnippetViewSet.as_view(
     {"get": "retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"}
 )
 
-snippet_highlight = SnippetViewSet.as_view({"get": "highlight"})
+snippet_highlight = SnippetViewSet.as_view(
+    {"get": "highlight"}, renderer_classes=[renderers.StaticHTMLRenderer]
+)
 
 user_list = UserViewSet.as_view({"get": "list"})
 user_detail = UserViewSet.as_view({"get": "retrieve"})
