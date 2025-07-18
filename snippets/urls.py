47896fbdba22ea60636db1461e31eb71e25143f3
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-07-18 17:55:31 +0600

    routers taking care of viewSet

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,36 +1,41 @@
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
-from rest_framework import renderers
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
 from snippets.views import (
     SnippetViewSet,
     UserViewSet,
-    api_root,
 )
 
-snippet_list = SnippetViewSet.as_view({"get": "list", "post": "create"})
+router = DefaultRouter()
+router.register(r"snippets", SnippetViewSet, basename="snippet")
+router.register(r"users", UserViewSet, basename="user")
+urlpatterns = [path("", include(router.urls))]
 
-snippet_detail = SnippetViewSet.as_view(
-    {"get": "retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"}
-)
 
-snippet_highlight = SnippetViewSet.as_view(
-    {"get": "highlight"}, renderer_classes=[renderers.StaticHTMLRenderer]
-)
+# snippet_list = SnippetViewSet.as_view({"get": "list", "post": "create"})
 
-user_list = UserViewSet.as_view({"get": "list"})
-user_detail = UserViewSet.as_view({"get": "retrieve"})
+# snippet_detail = SnippetViewSet.as_view(
+#     {"get": "retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"}
+# )
 
-urlpatterns = [
-    path("", api_root),
-    path("snippets/", snippet_list, name="snippet-list"),
-    path("snippets/<int:pk>/", snippet_detail, name="snippet-detail"),
-    path(
-        "snippets/<int:pk>/highlight/",
-        snippet_highlight,
-        name="snippet-highlight",
-    ),
-    path("users/", user_list, name="user-list"),
-    path("users/<int:pk>/", user_detail, name="user-detail"),
-]
+# snippet_highlight = SnippetViewSet.as_view(
+#     {"get": "highlight"}, renderer_classes=[renderers.StaticHTMLRenderer]
+# )
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+# user_list = UserViewSet.as_view({"get": "list"})
+# user_detail = UserViewSet.as_view({"get": "retrieve"})
+
+# urlpatterns = [
+#     path("", api_root),
+#     path("snippets/", snippet_list, name="snippet-list"),
+#     path("snippets/<int:pk>/", snippet_detail, name="snippet-detail"),
+#     path(
+#         "snippets/<int:pk>/highlight/",
+#         snippet_highlight,
+#         name="snippet-highlight",
+#     ),
+#     path("users/", user_list, name="user-list"),
+#     path("users/<int:pk>/", user_detail, name="user-detail"),
+# ]
+
+# urlpatterns = format_suffix_patterns(urlpatterns)
